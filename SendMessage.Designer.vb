cab70ac31a2baa765ffd364a8e2fb09bc4afe708
jcadmin <post@jcp.local>
2020-11-19 14:59:56 +0900

    other files (not certain).

--- SendMessage.Designer.vb
+++ SendMessage.Designer.vb
@@ -22,69 +22,53 @@ Partial Class SendMessage
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
-		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SendMessage))
-		Me.btnSend = New System.Windows.Forms.Button()
-		Me.btnCancel = New System.Windows.Forms.Button()
-		Me.txtMessage = New System.Windows.Forms.TextBox()
-		Me.Label1 = New System.Windows.Forms.Label()
-		Me.SuspendLayout()
-		'
-		'btnSend
-		'
-		Me.btnSend.Location = New System.Drawing.Point(191, 288)
-		Me.btnSend.Name = "btnSend"
-		Me.btnSend.Size = New System.Drawing.Size(75, 23)
-		Me.btnSend.TabIndex = 1
-		Me.btnSend.Text = "Send"
-		Me.btnSend.UseVisualStyleBackColor = True
-		'
-		'btnCancel
-		'
-		Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
-		Me.btnCancel.Location = New System.Drawing.Point(272, 288)
-		Me.btnCancel.Name = "btnCancel"
-		Me.btnCancel.Size = New System.Drawing.Size(75, 23)
-		Me.btnCancel.TabIndex = 2
-		Me.btnCancel.Text = "Cancel"
-		Me.btnCancel.UseVisualStyleBackColor = True
-		'
-		'txtMessage
-		'
-		Me.txtMessage.Location = New System.Drawing.Point(12, 31)
-		Me.txtMessage.Multiline = True
-		Me.txtMessage.Name = "txtMessage"
-		Me.txtMessage.Size = New System.Drawing.Size(335, 237)
-		Me.txtMessage.TabIndex = 0
-		'
-		'Label1
-		'
-		Me.Label1.AutoSize = True
-		Me.Label1.Location = New System.Drawing.Point(12, 15)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(77, 13)
-		Me.Label1.TabIndex = 3
-		Me.Label1.Text = "Send message"
-		'
-		'SendMessage
-		'
-		Me.AcceptButton = Me.btnSend
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.CancelButton = Me.btnCancel
-		Me.ClientSize = New System.Drawing.Size(370, 331)
-		Me.Controls.Add(Me.Label1)
-		Me.Controls.Add(Me.txtMessage)
-		Me.Controls.Add(Me.btnCancel)
-		Me.Controls.Add(Me.btnSend)
-		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-		Me.Name = "SendMessage"
-		Me.Text = "Send Message"
-		Me.ResumeLayout(False)
-		Me.PerformLayout()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SendMessage))
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.txtMessage = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSend
+        '
+        resources.ApplyResources(Me.btnSend, "btnSend")
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        resources.ApplyResources(Me.btnCancel, "btnCancel")
+        Me.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'txtMessage
+        '
+        resources.ApplyResources(Me.txtMessage, "txtMessage")
+        Me.txtMessage.Name = "txtMessage"
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'SendMessage
+        '
+        Me.AcceptButton = Me.btnSend
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnCancel
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSend)
+        Me.Name = "SendMessage"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
-	End Sub
+    End Sub
 
-	Friend WithEvents btnSend As Button
+    Friend WithEvents btnSend As Button
 	Friend WithEvents btnCancel As Button
 	Friend WithEvents txtMessage As TextBox
 	Friend WithEvents Label1 As Label
